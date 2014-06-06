315dbc4122751548b7578f2b38a51887118d5b0c
Subhojit Paul <subhojit_19@yahoo.in>
2014-06-07 01:22:54 +0530

    JavaScript behavior added
    - https://drupal.org/node/1844398#comment-8841721

--- js/dc_ajax_add_cart.js
+++ js/dc_ajax_add_cart.js
@@ -1,6 +1,10 @@
 (function ($, Drupal, window, document, undefined) {
-  $('.add-to-cart-close').live('click', function() {
-    $('#add-to-cart-overlay').fadeOut('fast');
-    $('.add-cart-message-wrapper').css('display', 'none');
-  });
+  Drupal.behaviors.dcAjaxAddCart = {
+    attach: function (context, settings) {
+      $('.add-to-cart-close').live('click', function() {
+        $('#add-to-cart-overlay').fadeOut('fast');
+        $('.add-cart-message-wrapper').css('display', 'none');
+      });
+    }
+  };
 })(jQuery, Drupal, this, this.document);

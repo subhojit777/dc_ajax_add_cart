c66635af986ee4517405a62456073f1f65fcaf93
joelpittet <joelpittet@160302.no-reply.drupal.org>
2015-09-16 17:15:15 +0530

    Issue #2484439 by joelpittet: Close overlay with escape key

--- js/dc_ajax_add_cart.js
+++ js/dc_ajax_add_cart.js
@@ -1,9 +1,18 @@
 (function ($, Drupal, window, document, undefined) {
   Drupal.behaviors.dcAjaxAddCart = {
     attach: function (context, settings) {
-      $('body').delegate('[data-dismiss="add-cart-message"]', 'click', function() {
+      // Close the add to cart overlay.
+      function closeAddToCartOverlay() {
         $('#add-to-cart-overlay').fadeOut('fast');
         $('.add-cart-message-wrapper').css('display', 'none');
+      }
+
+      $('body').delegate('[data-dismiss="add-cart-message"]', 'click', closeAddToCartOverlay);
+      $(document).keyup(function(e) {
+        // Close overlay with esc key.
+        if (e.keyCode == 27) {
+          closeAddToCartOverlay();
+        }
       });
     }
   };

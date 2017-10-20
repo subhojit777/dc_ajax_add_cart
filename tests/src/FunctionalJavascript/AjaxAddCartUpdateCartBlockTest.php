5de4e4a1bf92c9f9793b79f48e0eb865c26f0f14
Subhojit Paul <subhojitpaul21@gmail.com>
2017-10-20 12:16:12 +0530

    Issue #2910746 by subhojit777: Ajaxify the update of cart block

---
+++ tests/src/FunctionalJavascript/AjaxAddCartUpdateCartBlockTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\Tests\dc_ajax_add_cart\FunctionalJavascript;
+
+use Drupal\commerce_order\Entity\Order;
+use Drupal\Tests\dc_ajax_add_cart\Functional\AjaxAddCartTestBase;
+
+/**
+ * Ajax add cart update cart block tests.
+ *
+ * @TODO The default `testing` profile is unable to update the cart block. Find
+ * out why this is happening, and move this test inside `AjaxAddCartTest`. If
+ * you are able to do this, add another test just like `testAjaxAddCartForm()`
+ * that would test whether updating of cart block is indeed ajaxified.
+ *
+ * @ingroup dc_ajax_add_cart
+ *
+ * @group dc_ajax_add_cart
+ */
+class AjaxAddCartUpdateCartBlockTest extends AjaxAddCartTestBase {
+
+  /**
+   * Profile to be used for testing.
+   *
+   * @var string
+   */
+  protected $profile = 'standard';
+
+  /**
+   * Tests whether the cart block is updated after product added to cart.
+   */
+  public function testUpdateCartBlock() {
+    $this->drupalGet("product/{$this->variation->getProduct()->id()}");
+    $ajax_add_cart_button = $this->getSession()->getPage()->findButton('Add to cart');
+
+    $ajax_add_cart_button->click();
+    $this->waitForAjaxToFinish();
+
+    /*
+     * Confirm that the initial add to cart submit works.
+     */
+    $this->cart = Order::load($this->cart->id());
+    $order_items = $this->cart->getItems();
+    $this->assertOrderItemInOrder($this->variation, $order_items[0]);
+
+    // Confirm that the cart block has been updated.
+    $cart_block_contents = $this->xpath('//div[contains(@class, :class)]', [
+      ':class' => 'cart-block--contents',
+    ]);
+    $this->assertEquals(1, count($cart_block_contents), 'Cart block not updated.');
+  }
+
+}

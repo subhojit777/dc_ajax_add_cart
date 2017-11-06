5c3be037a5ea2ab966b50942a8a0f8613efbebb9
Subhojit Paul <subhojitpaul21@gmail.com>
2017-11-06 12:01:00 +0530

    Issue #2910746 by subhojit777: Ajaxify the update of cart block
    - Kernel tests for `getCartBlock()`, `updateCart()`,
      `updatePageElements()`.

--- tests/src/Kernel/RefreshPageElementsHelperTest.php
+++ tests/src/Kernel/RefreshPageElementsHelperTest.php
@@ -46,11 +46,30 @@ class RefreshPageElementsHelperTest extends CommerceKernelTestBase {
   protected $activeTheme;
 
   /**
-   * Ajax command names expected to be present in ajax response.
+   * Ajax command names expected to be present in status update ajax response.
    *
    * @var array
    */
-  protected $expectedAjaxCommandNames = [
+  protected $expectedAjaxCommandNamesStatusMessagesUpdate = [
+    'remove',
+    'insert',
+  ];
+
+  /**
+   * Ajax command names expected to be present in update cart ajax response.
+   *
+   * @var array
+   */
+  protected $expectedAjaxCommandNamesCartBlockUpdate = [
+    'insert',
+  ];
+
+  /**
+   * Ajax command names expected to be present when page elements updated.
+   *
+   * @var array
+   */
+  protected $expectedAjaxCommandNamesUpdatePageElements = [
     'remove',
     'insert',
   ];
@@ -76,14 +95,9 @@ class RefreshPageElementsHelperTest extends CommerceKernelTestBase {
   }
 
   /**
-   * Tests ajax response when status messages block is placed.
-   *
-   * @covers ::getStatusMessagesBlock
-   * @covers ::getResponse
-   * @covers ::updateStatusMessages
+   * Places status messages block.
    */
-  public function testAjaxResponseStatusMessagesBlock() {
-    // Place status messages block.
+  protected function placeStatusMessagesBlock() {
     $entity = $this->controller->create([
       'id' => "{$this->activeTheme->getName()}_messages",
       'theme' => $this->activeTheme->getName(),
@@ -91,6 +105,17 @@ class RefreshPageElementsHelperTest extends CommerceKernelTestBase {
       'plugin' => 'system_messages_block',
     ]);
     $entity->save();
+  }
+
+  /**
+   * Tests ajax response when status messages block is placed.
+   *
+   * @covers ::getStatusMessagesBlock
+   * @covers ::getResponse
+   * @covers ::updateStatusMessages
+   */
+  public function testAjaxResponseStatusMessagesBlock() {
+    $this->placeStatusMessagesBlock();
 
     $refereshPageElementsHelper = new RefreshPageElementsHelper(new AjaxResponse());
     $response = $refereshPageElementsHelper
@@ -104,7 +129,7 @@ class RefreshPageElementsHelperTest extends CommerceKernelTestBase {
       return $i['command'];
     }, $ajax_commands);
 
-    foreach ($this->expectedAjaxCommandNames as $ajax_command_name) {
+    foreach ($this->expectedAjaxCommandNamesStatusMessagesUpdate as $ajax_command_name) {
       $this->assertTrue(in_array($ajax_command_name, $actual_ajax_command_names), "$ajax_command_name is not present");
     }
   }
@@ -129,8 +154,63 @@ class RefreshPageElementsHelperTest extends CommerceKernelTestBase {
       return $i['command'];
     }, $ajax_commands);
 
-    foreach ($this->expectedAjaxCommandNames as $ajax_command_name) {
+    foreach ($this->expectedAjaxCommandNamesStatusMessagesUpdate as $ajax_command_name) {
       $this->assertFalse(in_array($ajax_command_name, $actual_ajax_command_names), "$ajax_command_name is present");
+    }
+  }
+
+  /**
+   * Tests ajax response when cart block is updated.
+   *
+   * @covers ::getCartBlock
+   * @covers ::updateCart
+   * @covers ::getResponse
+   */
+  public function testAjaxResponseCartBlock() {
+    $refereshPageElementsHelper = new RefreshPageElementsHelper(new AjaxResponse());
+    $response = $refereshPageElementsHelper
+      ->updateCart()
+      ->getResponse();
+    $this->assertAjaxResponse($response);
+
+    // Check if the returned response has the expected ajax commands.
+    $ajax_commands = $response->getCommands();
+    $actual_ajax_command_names = array_map(function ($i) {
+      return $i['command'];
+    }, $ajax_commands);
+
+    foreach ($this->expectedAjaxCommandNamesCartBlockUpdate as $ajax_command_name) {
+      $this->assertTrue(in_array($ajax_command_name, $actual_ajax_command_names), "$ajax_command_name is not present");
+    }
+  }
+
+  /**
+   * Tests updatePageElements().
+   *
+   * @covers ::getStatusMessagesBlock
+   * @covers ::updateStatusMessages
+   * @covers ::getCartBlock
+   * @covers ::updateCart
+   * @covers ::getResponse
+   */
+  public function testAjaxResponseUpdatePageElements() {
+    $this->placeStatusMessagesBlock();
+
+    $refereshPageElementsHelper = new RefreshPageElementsHelper(new AjaxResponse());
+    $refreshPageElements = $refereshPageElementsHelper
+      ->updatePageElements();
+    $this->assertInstanceOf(RefreshPageElementsHelper::class, $refreshPageElements, 'Not an instance of RefreshPageElementsHelper');
+    $response = $refreshPageElements->getResponse();
+    $this->assertAjaxResponse($response);
+
+    // Check if the returned response has the expected ajax commands.
+    $ajax_commands = $response->getCommands();
+    $actual_ajax_command_names = array_map(function ($i) {
+      return $i['command'];
+    }, $ajax_commands);
+
+    foreach ($this->expectedAjaxCommandNamesUpdatePageElements as $ajax_command_name) {
+      $this->assertTrue(in_array($ajax_command_name, $actual_ajax_command_names), "$ajax_command_name is not present");
     }
   }
 

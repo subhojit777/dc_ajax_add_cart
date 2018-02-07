b85d66371d4329066a5d41dda8f56654e9ab30f6
Subhojit Paul <subhojitpaul21@gmail.com>
2018-02-07 11:51:28 +0530

    Issue #2910741 by subhojit777: Ajaxify the Update cart button
    - Added more tests:
      - Test whether the update cart button is indeed ajaxified.
      - Test whether the order total is correct after quantity update.

--- modules/dc_ajax_add_cart_views/tests/src/FunctionalJavascript/AjaxAddCartViewsEditQuantityTest.php
+++ modules/dc_ajax_add_cart_views/tests/src/FunctionalJavascript/AjaxAddCartViewsEditQuantityTest.php
@@ -46,4 +46,67 @@ class AjaxAddCartViewsEditQuantityTest extends AjaxAddCartViewsTestBase {
     $this->assertVariationInOrder($cart_variation, $order_items, 2);
   }
 
+  /**
+   * Tests whether the update cart button is indeed ajaxified.
+   */
+  public function testAjaxUpdateCartButton() {
+    $this->drupalLogin($this->account);
+
+    $cart_variation = array_pop($this->variations);
+    $this->cartManager->addEntity($this->cart, $cart_variation);
+
+    $this->drupalGet("cart-update-ajax/{$this->cart->id()}");
+    $this->assertCartAjaxPage();
+
+    $variation_row_element = $this->getRowCartAjaxByVariation($cart_variation);
+    $this->assertVariationRowCartAjax($variation_row_element);
+    $variation_position = $this->getVariationRowPositionCartAjax($cart_variation);
+    $this->assertVariationRowPosition($variation_position);
+
+    $variation_row_element->fillField("dc_ajax_add_cart_views_edit_quantity[{$variation_position}]", '2');
+
+    $this->getSession()->getPage()->findButton('Update cart')
+      ->click();
+
+    $this->cart = Order::load($this->cart->id());
+    $order_items = $this->cart->getItems();
+
+    // Check whether the product quantity is still the same.
+    $variation_row_element = $this->getRowCartAjaxByVariation($cart_variation);
+    $this->assertVariationRowCartAjax($variation_row_element);
+    $this->assertVariationInOrder($cart_variation, $order_items);
+  }
+
+  /**
+   * Tests whether order total is correct on ajax quantity update.
+   */
+  public function testOrderTotal() {
+    $this->drupalLogin($this->account);
+
+    $cart_variation = array_pop($this->variations);
+    $this->cartManager->addEntity($this->cart, $cart_variation);
+
+    $this->drupalGet("cart-update-ajax/{$this->cart->id()}");
+    $this->assertCartAjaxPage();
+
+    $variation_row_element = $this->getRowCartAjaxByVariation($cart_variation);
+    $this->assertVariationRowCartAjax($variation_row_element);
+    $variation_position = $this->getVariationRowPositionCartAjax($cart_variation);
+    $this->assertVariationRowPosition($variation_position);
+
+    $variation_row_element->fillField("dc_ajax_add_cart_views_edit_quantity[{$variation_position}]", '2');
+
+    $this->getSession()->getPage()->findButton('Update cart')
+      ->click();
+    $this->waitForAjaxToFinish();
+
+    $this->cart = Order::load($this->cart->id());
+    $order_items = $this->cart->getItems();
+
+    $price = $this->getSession()->getPage()->find('css', '.order-total-line__total .order-total-line-value')->getText();
+    $price = (float) preg_replace('/[^0-9\.]/', '', $price);
+    $actual_price = (float) $this->cart->getTotalPrice()->getNumber();
+    $this->assertEquals($price, $actual_price, 'Prices are not equal.');
+  }
+
 }
